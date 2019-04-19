1a5d8bc36dbb39e0a7a18cb5ffefa34e06a77854
nerd553 <geomelkon@csd.auth.gr>
2019-04-19 21:00:25 +0300

    Deployment build

---
+++ auth/precache-manifest.3465947b738ee91b8a6b12dcb2e18842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "713fe29d10415a51910d79f66c21d161",
+    "url": "/auth/static/media/back-btn.713fe29d.svg"
+  },
+  {
+    "revision": "d9022639686edba99362",
+    "url": "/auth/static/js/runtime~main.7372c815.js"
+  },
+  {
+    "revision": "50c3be0b344871332078",
+    "url": "/auth/static/js/main.fb657215.chunk.js"
+  },
+  {
+    "revision": "f9dd4608903bc87ed305",
+    "url": "/auth/static/js/2.825a8842.chunk.js"
+  },
+  {
+    "revision": "50c3be0b344871332078",
+    "url": "/auth/static/css/main.ee4dc80e.chunk.css"
+  },
+  {
+    "revision": "97e404d659d10d14dbb57b7da5b6c7be",
+    "url": "/auth/index.html"
+  }
+];
---
+++ sorter/precache-manifest.7c3d4a568abc9e665fcd1659e1a4d640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0aa27c8de5228d5d9569",
+    "url": "./sort/static/js/runtime~main.9c5ea4d8.js"
+  },
+  {
+    "revision": "f073cbcba5ca85236663",
+    "url": "./sort/static/js/main.5f7de440.chunk.js"
+  },
+  {
+    "revision": "e46a1f331f1548883b91",
+    "url": "./sort/static/js/2.3dd8b941.chunk.js"
+  },
+  {
+    "revision": "f073cbcba5ca85236663",
+    "url": "./sort/static/css/main.7378f06e.chunk.css"
+  },
+  {
+    "revision": "5aaa50f2e9abcfa0b4ddda490de995de",
+    "url": "./sort/index.html"
+  }
+];
