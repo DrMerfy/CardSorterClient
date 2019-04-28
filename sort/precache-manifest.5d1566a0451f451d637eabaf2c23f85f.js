01b540252b11daa76050867fe1e9e1a90d0d19e1
nerd553 <geomelkon@csd.auth.gr>
2019-04-28 16:59:44 +0300

    Routing fix 3

---
+++ sort/precache-manifest.5d1566a0451f451d637eabaf2c23f85f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "30028bfd852bacb83f3f0c268754dc26",
+    "url": "./sort/static/media/down-arrow-sketch.30028bfd.svg"
+  },
+  {
+    "revision": "e43a77a74a3e02389b72",
+    "url": "./sort/static/css/main.4ac6a923.chunk.css"
+  },
+  {
+    "revision": "0aa27c8de5228d5d9569",
+    "url": "./sort/static/js/runtime~main.9c5ea4d8.js"
+  },
+  {
+    "revision": "1fe8e545b59fce3eb152",
+    "url": "./sort/static/js/2.cbae756e.chunk.js"
+  },
+  {
+    "revision": "ed718a5fe4c2c476c20d3e21a9f22868",
+    "url": "./sort/static/media/plus.ed718a5f.svg"
+  },
+  {
+    "revision": "e43a77a74a3e02389b72",
+    "url": "./sort/static/js/main.897c665f.chunk.js"
+  },
+  {
+    "revision": "05e07b5d82c55994fbc55284f47353f4",
+    "url": "./sort/static/media/hello.05e07b5d.svg"
+  },
+  {
+    "revision": "84d1d4ad9a02274f3e3abdbea834dad9",
+    "url": "./sort/static/media/thanks-icon.84d1d4ad.svg"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "./sort/static/media/not-found.8c3d2a79.svg"
+  },
+  {
+    "revision": "7a0423bd6098151cfe14f7d230cff8c5",
+    "url": "./sort/static/media/verified.7a0423bd.svg"
+  },
+  {
+    "revision": "253cc403e3784529580b768f3fe1e946",
+    "url": "./sort/index.html"
+  }
+];
